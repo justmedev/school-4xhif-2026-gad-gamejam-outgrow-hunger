52e73655190acbda363b10206890c565389627d8
Pes220212 <pes220212@spengergasse.at>
2026-01-28 08:29:06 +0100

    disable player in night

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -6,16 +6,22 @@ public class GameStateManager : MonoBehaviour
 {
     public int CurrentDay { get; private set; }
     private GameUIController _gameUIController;
+    private PlayerMovement _playerMovement;
+    private House _house;
 
     void Start()
     {
         _gameUIController = FindFirstObjectByType<GameUIController>();
+        _playerMovement = FindFirstObjectByType<PlayerMovement>();
+        _house = FindFirstObjectByType<House>();
     }
     
     public void NextDay()
     {
         _gameUIController.SwitchToNightUI();
-
+        _playerMovement.canMove = false;
+        _house.canSleep = false;
+        
         StartCoroutine(NightScene());
         
         CurrentDay++;
@@ -26,5 +32,7 @@ public class GameStateManager : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         _gameUIController.SwitchToDayUI();
+        _playerMovement.canMove = true;
+        _house.canSleep = true;
     }
 }
