59e164ef8e901f52d3b69783fcfeca95e0a170ba
Pes220212 <167974238+Pes220212@users.noreply.github.com>
2026-01-29 10:52:50 +0100

    fix pause menu and build (#46)
    * fix pause menu and build
    
    * Add Lose Background
    
    * Add House to Scene

---
+++ Assets/Scripts/UI/Lose/BackgroundResizer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace UI.Lose
+{
+    public class BackgroundResizer : MonoBehaviour
+    {
+        public float scaleMultiplier = 1.1f;
+
+        private Camera _cam;
+        private RectTransform _rectTransform;
+        private SpriteRenderer _spriteRenderer;
+
+        void Awake()
+        {
+            _cam = Camera.main;
+            _rectTransform = GetComponent<RectTransform>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void LateUpdate()
+        {
+            if (!_cam) return;
+
+            if (_rectTransform)
+            {
+                _rectTransform.sizeDelta = new Vector2(
+                    Screen.width * scaleMultiplier,
+                    Screen.height * scaleMultiplier
+                );
+            }
+            else if (_spriteRenderer && _cam.orthographic)
+            {
+                float camHeight = _cam.orthographicSize * 2f;
+                float camWidth = camHeight * _cam.aspect;
+
+                Vector2 spriteSize = _spriteRenderer.sprite.bounds.size;
+
+                transform.localScale = new Vector3(
+                    camWidth / spriteSize.x * scaleMultiplier,
+                    camHeight / spriteSize.y * scaleMultiplier,
+                    1f
+                );
+            }
+        }
+    }
+}
